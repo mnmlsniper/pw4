727cbbd700c46851246128f06e8d48abd946a6d1
Your Name <you@example.com>
2025-09-05 22:20:29 +0300

    add page object

---
+++ src/pages/register.page.js
@@ -0,0 +1,25 @@
+export class RegisterPage {
+	constructor(page) {
+		// техническое описание страницы
+		//todo нейминг
+		this.nameInput = page.getByRole('textbox', { name: 'Your Name' });
+		this.emailInput = page.getByRole('textbox', { name: 'Email' });
+		this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+		this.signupButton = page.getByRole('button', { name: 'Sign up' });
+		this.emailErrorText = page.getByText(
+			'Email already exists.. try logging in',
+		);
+	}
+	// бизнесовые действия со страницой
+	async register(user) {
+		const { name, email, password } = user;
+		await this.nameInput.click();
+		await this.nameInput.fill(name);
+		await this.emailInput.click();
+		//todo хардкод
+		await this.emailInput.fill('dlove@yandex.ru');
+		await this.passwordInput.click();
+		await this.passwordInput.fill(password);
+		await this.signupButton.click();
+	}
+}
---
+++ tests/5lesson.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import { MainPage, RegisterPage } from '../src/pages/index';
+
+const URL = 'https://realworld.qa.guru/';
+
+test.describe('Регистрация', () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto(URL);
+	});
+
+	test.only('Пользователь не может зарегистрироваться повторно', async ({
+		page,
+	}) => {
+		const user = {
+			name: faker.person.fullName(),
+			email: faker.internet.email(),
+			password: faker.internet.password(),
+		};
+
+		const mainPage = new MainPage(page);
+		const registerPage = new RegisterPage(page);
+
+		await mainPage.gotoRegister();
+		await registerPage.register(user);
+
+		// todo переделать ассерт
+		await expect(registerPage.emailErrorText).toContainText(
+			'Email already exists.. try logging in',
+		);
+	});
+});
