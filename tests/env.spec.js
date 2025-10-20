2879afb19a2bec272741ebdb32ee45aaa3d53415
sniper <mnml.sniper@gmail.com>
2025-10-20 21:15:21 +0300

    fix

--- tests/env.spec.js
+++ tests/env.spec.js
@@ -4,6 +4,10 @@ test('test', async ({ page }) => {
 	// arrange или настройка, предусловие
 	await page.goto('https://todomvc.com/examples/vue/dist/#/');
     console.log(process.env);
+    console.log(process.env.secret);
+    console.log(process.env.step);
+
+
 	await expect(
 		page.getByRole('textbox', { name: 'What needs to be done?' }),
 	).toBeEmpty();
