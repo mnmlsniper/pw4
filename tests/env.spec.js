288c10abe28fcbdb7d380a781d5731dbcca71d74
sniper <mnml.sniper@gmail.com>
2025-10-20 21:22:43 +0300

    add env

--- tests/env.spec.js
+++ tests/env.spec.js
@@ -4,8 +4,8 @@ test('test', async ({ page }) => {
 	// arrange или настройка, предусловие
 	await page.goto('https://todomvc.com/examples/vue/dist/#/');
     console.log(process.env);
-    console.log(process.env.secret);
-    console.log(process.env.step);
+    console.log(process.env.SECRET);
+    console.log(process.env.STEP);
 
 
 	await expect(
